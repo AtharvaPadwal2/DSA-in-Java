66faf94043c136cf6113451611f0e044af5013f8
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-11 23:53:33 +0530

    feat: add variables and data types example

---
+++ java_basics/VariablesAndDataTypes.java
@@ -0,0 +1,19 @@
+
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+       String name = "Atharva";
+       int age = 20;
+       double gpa = 8.67;
+       boolean DSA = true ;
+       char Grade = 'A';
+
+       System.out.println("Name :"+ name);
+       System.out.println("Age :"+ age);
+       System.out.println("CGPA :"+ gpa);
+       System.out.println("Learning DSA :"+ DSA);
+       System.out.println("Grade :"+ Grade);
+
+
+    }
+}
