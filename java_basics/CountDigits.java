6c026d3cd80483b0029a9af3dfda7f5d30f72292
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-12 13:51:34 +0530

    Added a program to count digits

---
+++ java_basics/CountDigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+
+       int count = 0;
+int temp = num;
+
+if (temp == 0) {
+    count = 1;
+} else {
+    while (temp != 0) {
+        temp = temp / 10;
+        count++;
+    }
+}
+
+        System.out.println("The length of the entered number is: " + count);
+        sc.close();
+    }
+}
