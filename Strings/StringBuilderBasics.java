28bbdec8a587e57dce8e63e97838bf3a2b0f41bf
Atharva Padwal <atharvapadwal520@gmail.com>
2026-09-18 21:23:10 +0530

    feat(strings): add StringBuilder basics

---
+++ Strings/StringBuilderBasics.java
@@ -0,0 +1,32 @@
+package Strings;
+
+import java.util.*;
+
+public class StringBuilderBasics {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String : ");
+        
+       
+        StringBuilder sb = new StringBuilder(sc.nextLine());
+
+        if (sb.isEmpty()) {
+          System.out.println("Invalid Input");
+        } else {
+        
+        sb.append("Atharva");
+        sb.setCharAt(1, 'a');
+
+        System.out.println(sb);
+
+        System.out.println(sb.insert(1, 'a'));
+
+        System.out.println(sb.deleteCharAt(1));
+
+        System.out.println(sb.reverse());
+ 
+        }
+        sc.close();
+    }
+}
